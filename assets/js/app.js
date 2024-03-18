3a667bfb90a5e31522ac8302eef5565e4d085c31
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-03-19 01:11:18 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    var loading = document.getElementById('loading');
+    var content = document.getElementById('content');
+    
+    loading.style.display = 'none';
+    content.style.display = 'block';
+  });
+  
